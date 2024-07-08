af6f67c4c53b38b6dec3e8c177bd3fb9b00e6a85
biancaleitao <biancacosta.le@hotmail.com>
2024-07-08 12:21:35 -0300

    Project QXBack

---
+++ LandingPage/reveal.js
@@ -0,0 +1,42 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.efeito-txt-home',{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-bttn-home',{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-txt-instruction',{
+    duration: 2000,
+    distance: '90px',
+    delay: 0.5
+})
+
+revelar.reveal('.efeito-txt-content',{
+    duration: 1000,
+    distance: '90px',
+    delay: 500,
+    origin: 'right'
+})
+
+revelar.reveal('.efeito-card1',{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-card2',{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-card3',{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
