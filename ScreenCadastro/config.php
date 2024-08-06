5cdd681964f61ca19a12977e921d77acd49d1dbe
biancaleitao <biancacosta.le@hotmail.com>
2024-08-06 14:26:30 -0300

    Create config.php

---
+++ ScreenCadastro/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '3p01nT2024#';
+    $dbName = 'formulario_cadastro';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    /*if ($conexao->connect_errno){
+        echo "ERRO";
+    }
+    else{
+        echo "Conexão efetuada com sucesso!";
+    } */
+?>
